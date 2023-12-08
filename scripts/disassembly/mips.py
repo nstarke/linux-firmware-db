a736cf0f8d4a1702ae0437145a42b20318b6082e
nstarke <starke.nicholas@gmail.com>
2023-12-08 03:06:45 +0000

    more disassembly

--- scripts/disassembly/mips.py
+++ scripts/disassembly/mips.py
@@ -2,19 +2,24 @@
 
 import sys, json, os, r2pipe
 tag = sys.argv[1]
+check_file = True
+if len(sys.argv) == 3:
+    print("Skipping file check")
+    check_file = False
 
 with open("../data/json/linux-firmware-db-" + tag + "-cpu_rec.json", 'r') as f:
     j = json.loads(f.read())
     for i in j:
         if 'mips' in i['full_arch'].lower():
             e = i['full_arch'][-2:]
-            endian = "false"
-            if e == 'eb':
+            endian = None
+            if e == 'eb' or e == 'EB':
                 endian = "true"
-            if not os.path.isfile("../data/txt/disassembly/linux-firmware-db-" + i['sha256'] + '-disassembly.txt'):
+            if not os.path.isfile("../data/txt/disassembly/linux-firmware-db-" + i['sha256'] + '-disassembly.txt') or not check_file:
                 try:
                     R2 = r2pipe.open("../git/linux-firmware/" + i['file_name'])
-                    R2.cmd("e cfg.bigendian = " + endian)
+                    if endian:
+                        R2.cmd("e cfg.bigendian = " + endian)
                     R2.cmd("e asm.arch = mips")
                     R2.cmd("aaaa")
                     R2.cmd('pdr @@f > ' + "../data/txt/disassembly/linux-firmware-db-" + i['sha256'] + '-disassembly.txt')
